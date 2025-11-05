9a8d40be26afbed53f73196b5f03d3eaa886fcc2
meljz <reyesmeljohnjay@gmail.com>
2025-11-05 23:17:59 +0800

    fix home layout responsiveness

--- static/angeles_news/js/script.js
+++ static/angeles_news/js/script.js
@@ -11,3 +11,11 @@ const track = document.querySelector('.carousel-track');
             index = (index - 1 + slides.length) % slides.length;
             track.style.transform = `translateX(-${index * 100}%)`;
         };
+/*this will serve as the DOM of the burger, without this burger will not be clickable */
+const burger = document.querySelector('.burger_menu');
+  const navLinks = document.querySelector('.nav-links');
+
+  burger?.addEventListener('click', () => {
+    navLinks?.classList.toggle('active');
+  });
+        
