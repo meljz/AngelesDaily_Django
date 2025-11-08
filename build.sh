f833ef6e38aff3dafce401cfe28996e0214a7651
meljz <reyesmeljohnjay@gmail.com>
2025-11-08 18:18:15 +0800

    Ensure static files are collected for Render

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-bash#!/usr/bin/env bash
+#!/usr/bin/env bash
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+python manage.py collectstatic --noinput
 python manage.py migrate
